24c238ca7bd9d5c21465ee3679f545e5dd6d7338
AllusiveGem <reichertcorey08@gmail.com>
2025-02-04 00:26:30 -0500

    Homework

---
+++ HW3/buildingpoints.js
@@ -0,0 +1,215 @@
+var stadiumsGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bank of America Stadium",
+          "note": "Bank of America Stadium (Charlotte, NFL Carolina Panthers)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.8526, 35.2251]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Truist Field",
+          "note": "Truist Field (Charlotte, Minor League Baseball - Charlotte Knights)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.8487, 35.2284]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Durham Bulls Athletic Park",
+          "note": "Durham Bulls Athletic Park (Durham, Minor League Baseball - Durham Bulls)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.903916, 35.991458]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "WakeMed Soccer Park",
+          "note": "WakeMed Soccer Park (Cary, Minor League Soccer - North Carolina FC)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.755107, 35.786128]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Atrium Health Ballpark",
+          "note": "Atrium Health Ballpark (Kannapolis, Minor League Baseball - Kannapolis Cannon Ballers)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.627096, 35.498270]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "First National Bank Field",
+          "note": "First National Bank Field (Greensboro, Minor League Baseball - Greensboro Grasshoppers)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-79.794774, 36.076565]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kenan Stadium",
+          "note": "Kenan Stadium (Chapel Hill, College Football - UNC Tar Heels)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-79.047993, 35.906847]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "McCormick Field",
+          "note": "McCormick Field (Asheville, Minor League Baseball - Asheville Tourists)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-82.549354, 35.587185]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "L.P. Frans Stadium",
+          "note": "L.P. Frans Stadium (Hickory, Minor League Baseball - Hickory Crawdads)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-81.378822, 35.749075]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sims Legion Park",
+          "note": "Sims Legion Park (Gastonia, Former Minor League Baseball - Gastonia Grizzlies (Now SpartanBurgers))"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-81.183813, 35.279810]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Truist Stadium",
+          "note": "Truist Stadium (Winston-Salem, Minor League Baseball - Winston-Salem Dash)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.255962, 36.091602]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Segra Park",
+          "note": "Segra Park (Fayetteville, Minor League Baseball - Fayetteville Woodpeckers)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.883382, 35.055914]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Five County Stadium",
+          "note": "Five County Stadium (Zebulon, Minor League Baseball - Carolina Mudcats)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.270518, 35.817132]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Smith's Ballpark",
+          "note": "Smith's Ballpark (Kinston, Minor League Baseball - Former Down East Wood Ducks)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-77.574167, 35.27]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bank of America Stadium",
+          "note": "Bank of America Stadium (Charlotte, Charlotte FC MLS)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.852778, 35.225833]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wallace Wade Stadium",
+          "note": "Wallace Wade Stadium (Durham, College Football Duke Blue Devils)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.941667, 35.995278]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Truist Point",
+          "note": "Truist Point (High Point, Minor League Baseball - High Point Rockers)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.011944, 35.959444]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Lenovo Center",
+          "note": "Lenovo Center (Raleigh, National Hockey League (NHL) - Carolina Hurricanes)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-78.721944, 35.803333]
+        }
+      },
+  
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Spectrum Center",
+          "note": "Spectrum Center (Charlotte, Charlotte Hornets NBA)"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-80.839167, 35.225]
+        }
+      }
+    ]
+  }
